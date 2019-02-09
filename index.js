4aafd7eda61d82dc4cafccce84f454d382272ff5
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2019-02-09 14:33:23 +0200

    First version

---
+++ index.js
@@ -0,0 +1,42 @@
+const ws = require('ws')
+
+const port = 7777
+
+let server = new ws.Server({
+    port
+})
+
+function log(...args) {
+    console.log(...args)
+}
+
+let clients = []
+
+let clientIdCounter = 0
+
+server.on('connection', (client) => {
+    client._id = clientIdCounter++
+
+    log(`Client ${client._id} connected`)
+
+    clients.push(client)
+
+    client.on('message', (message) => {
+        log(`${client._id} ${message}`)
+        for (let c of clients) {
+            if (c !== client) {
+                c.send(message)
+            }
+        }
+    })
+
+    client.on('close', () => {
+        log(`Client ${client._id} disconnected`)
+
+        let clientIndex = clients.indexOf(client)
+        clients.splice(clientIndex, 1)
+
+        log(`I have ${clients.length} client(s) now`)
+    })
+})
+
