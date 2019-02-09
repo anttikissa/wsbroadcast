9c3e0cb8aa041ab39452fb8a38e9f03d987d2c54
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2019-02-09 15:13:44 +0200

    Add room support
    Clients connecting to ws://localhost:7777/<room> can only listen to
    other clients that connected to the same room.

--- index.js
+++ index.js
@@ -10,20 +10,36 @@ function log(...args) {
     console.log(...args)
 }
 
-let clients = []
+// A room is an array of clients.
+//
+// Rooms is indexed by the path (such as '/' or '/room1') that the client used to connect to the
+// websocket.
+//
+// The default path is '/'.
+//
+// Rooms are not cleaned up even after the last client disconnects.
+let rooms = {}
+
+function getRoom(path) {
+    return rooms[path] || (rooms[path] = [])
+}
 
 let clientIdCounter = 0
 
-server.on('connection', (client) => {
+server.on('connection', (client, req) => {
     client._id = clientIdCounter++
 
-    log(`Client ${client._id} connected`)
+    let roomName = req.url
 
-    clients.push(client)
+    log(`Client ${client._id} connected to ${roomName}`)
+
+    let room = getRoom(roomName)
+
+    room.push(client)
 
     client.on('message', (message) => {
-        log(`<${client._id}> ${message}`)
-        for (let c of clients) {
+        log(`${roomName} <${client._id}> ${message}`)
+        for (let c of room) {
             if (c !== client) {
                 c.send(message)
             }
@@ -33,10 +49,10 @@ server.on('connection', (client) => {
     client.on('close', () => {
         log(`Client ${client._id} disconnected`)
 
-        let clientIndex = clients.indexOf(client)
-        clients.splice(clientIndex, 1)
+        let clientIndex = room.indexOf(client)
+        room.splice(clientIndex, 1)
 
-        log(`I have ${clients.length} client(s) now`)
+        log(`Room ${roomName} has ${room.length} client(s) now`)
     })
 })
 
